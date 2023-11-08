7d8dc6e850e553ce0c73026503c74493e0ba1cbd
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-08 17:16:34 +0100

    JSON

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             const response = await fetch(`https://testing-mauve-five.vercel.app/api/current_user`);
             console.log(response);
             console.log("/////////////////");
-            const json = await response.json()
+            const json = await response.JSON()
             console.log(json);
         };
         fetchData();
