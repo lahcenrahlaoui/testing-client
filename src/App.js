93f07bea766281086258eec716343eb9363ab762
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-08 22:48:26 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import { useEffect } from "react";
 import axios from "axios";
 function App() {
-    // const local = "http://locahsot:5000";
     const proxy = "https://testing-mauve-five.vercel.app";
     useEffect(() => {
         const fetchData = async () => {
-            // const ares = await axios.get("/api/current_user");
-            const response = await axios.get(`https://testing-mauve-five.vercel.app/api/current_user`);
+            const response = await axios.get(
+                `https://testing-mauve-five.vercel.app/api/current_user`
+            );
             console.log(response);
             console.log("/////////////////");
-            const json = await response.data
-            console.log(json);
         };
         fetchData();
     });
