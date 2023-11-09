0b481761f997cf9579282c52990edbca248f5f9a
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-09 20:02:03 +0100

    change links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 
-import Cookies from "js-cookie";
-
- 
 function App() {
-    const [state, setState] = useState({ user: Cookies.get("session") });
-    const proxy = "https://server-2nfv.onrender.com/api/current_user";
+    const proxy = "https://testing-mauve-five.vercel.app";
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(
-                `https://server-2nfv.onrender.com/api/current_user`
+                `https://testing-mauve-five.vercel.app/api/current_user`
             );
             console.log(response);
-            console.log("/||||||||||\\\\\\\\\\\\\\\|||///");
-            console.log(state);
+            console.log("/||||||||||\\\\\\\\\\\\\\|||///");
         };
         fetchData();
     });
@@ -31,7 +26,6 @@ function App() {
         <div className="App">
             <a href={`${proxy}/auth/google`}>login</a>
 
-       
             <div>---------------</div>
             <a href={`${proxy}/api/logout`}>logout</a>
         </div>
