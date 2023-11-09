88ad70c39abf09816f804241134218164b12a7a1
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-09 18:01:50 +0100

    change server

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import axios from "axios";
 
 import Cookies from "js-cookie";
 
-import { GoogleLoginButton } from "react-social-login-buttons";
-
+ 
 function App() {
     const [state, setState] = useState({ user: Cookies.get("session") });
-    const proxy = "https://testing-mauve-five.vercel.app";
+    const proxy = "https://server-2nfv.onrender.com/api/current_user";
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(
-                `https://testing-mauve-five.vercel.app/api/current_user`
+                `https://server-2nfv.onrender.com/api/current_user`
             );
             console.log(response);
             console.log("/|||||||||||||||||||///");
@@ -32,11 +31,7 @@ function App() {
         <div className="App">
             <a href={`${proxy}/auth/google`}>login</a>
 
-            <div>---------------</div>
-            <GoogleLoginButton
-                href={`${proxy}/auth/google`}
-                onClick={() => console.log("Hello")}
-            />
+       
             <div>---------------</div>
             <a href={`${proxy}/api/logout`}>logout</a>
         </div>
