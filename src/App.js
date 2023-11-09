6d09ee94105908a55e2fd26c028c34042d8e1820
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-09 22:30:24 +0100

    add proxy

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
-    const proxy = "https://testing-mauve-five.vercel.app";
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(
-                `https://testing-mauve-five.vercel.app/api/current_user`
+                `/api/current_user`
             );
             console.log(response);
             console.log("/||||||||||\\\\\\\\\\\\\\|||///");
@@ -15,19 +14,16 @@ function App() {
         fetchData();
     });
 
-    const googleSuccess = (res) => {
-        console.log(res);
-    };
-    const googleFailure = () => {
-        console.log("fail");
-    };
-
     return (
         <div className="App">
-            <a href={`${proxy}/auth/google`}>login</a>
+            <a href={`/auth/google`}>login</a>
 
             <div>---------------</div>
-            <a href={`${proxy}/api/logout`}>logout</a>
+            <a href={`/api/logout`}>logout</a>
+            {/* <a href={`${proxy}/auth/google`}>login</a>
+
+            <div>---------------</div>
+            <a href={`${proxy}/api/logout`}>logout</a> */}
         </div>
     );
 }
