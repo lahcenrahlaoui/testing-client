d22445270471aab0bfff81432e7b1d6d7e3a6526
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-12 20:55:57 +0100

    make the envirement development and production

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import axios from "axios";
 import Header from "./Header";
 function App() {
     const proxy = "https://testing-mauve-five.vercel.app";
-    const local = "https://localhost:5000";
+    const local = "http://localhost:5000";
 
     const server = process.env.NODE_ENV === "production" ? proxy : local;
 
-    console.log(process.env.NODE_ENV);
     const [user, setUser] = useState();
     useEffect(() => {
         const fetchUser = async () => {
             let response;
+            
+            
             if (process.env.NODE_ENV === "development") {
-                response = await axios.get(`/api/current_user`, {
-                    withCredentials: true,
-                });
+                response = await axios.get(`/api/current_user`);
             } else {
                 response = await axios.get(
                     `https://testing-mauve-five.vercel.app/api/current_user`,
@@ -25,7 +24,7 @@ function App() {
                     }
                 );
             }
-
+            console.log(response);
             setUser(response.data);
         };
 
--- src/Header.js
+++ src/Header.js
@@ -1,54 +1,3 @@
-// import React from "react";
-
-// const Header = ({ user }) => {
-//     return (
-//         <nav>
-//             <div className="nav-wrapper">
-//                 <a href="#" className="brand-logo">
-//                     Logo
-//                 </a>
-//                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-//                     {!user ? (
-//                         ""
-//                     ) : (
-//                         <>
-//                             <li>
-//                                 <img
-//                                     width={20}
-//                                     className="circle "
-//                                     src={user.photos}
-//                                 />
-//                             </li>
-//                             <li>
-//                                 <div>{user.name}</div>
-//                             </li>
-//                         </>
-//                     )}
-//                     <li>
-//                         {!user ? (
-//                             <a
-//                                 href={`https://testing-mauve-five.vercel.app/auth/google`}
-//                                 className="waves-effect waves-light btn"
-//                             >
-//                                 login
-//                             </a>
-//                         ) : (
-//                             <a
-//                                 href={`https://testing-mauve-five.vercel.app/api/logout`}
-//                                 className="waves-effect red accent-2 btn"
-//                             >
-//                                 logout
-//                             </a>
-//                         )}
-//                     </li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default Header;
-
 import React from "react";
 
 const Header = ({ user, server }) => {
@@ -58,6 +7,7 @@ const Header = ({ user, server }) => {
                 <a href="#" className="brand-logo">
                     Logo
                 </a>
+
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     {!user ? (
                         ""
@@ -99,3 +49,58 @@ const Header = ({ user, server }) => {
 };
 
 export default Header;
+
+
+
+
+// import React from "react";
+
+// const Header = ({ user }) => {
+//     return (
+//         <nav>
+//             <div className="nav-wrapper">
+//                 <a href="#" className="brand-logo">
+//                     Logo
+//                 </a>
+
+//                 <ul id="nav-mobile" className="right hide-on-med-and-down">
+//                     {!user ? (
+//                         ""
+//                     ) : (
+//                         <>
+//                             <li>
+//                                 <img
+//                                     width={20}
+//                                     className="circle "
+//                                     src={user.photos}
+//                                 />
+//                             </li>
+//                             <li>
+//                                 <div>{user.name}</div>
+//                             </li>
+//                         </>
+//                     )}
+//                     <li>
+//                         {!user ? (
+//                             <a
+//                                 href={`https://localhost:5000/auth/google`}
+//                                 className="waves-effect waves-light btn"
+//                             >
+//                                 login
+//                             </a>
+//                         ) : (
+//                             <a
+//                                 href={`https://localhost:5000/api/logout`}
+//                                 className="waves-effect red accent-2 btn"
+//                             >
+//                                 logout
+//                             </a>
+//                         )}
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>
+//     );
+// };
+
+// export default Header;
