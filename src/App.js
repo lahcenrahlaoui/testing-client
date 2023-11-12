db02a693274f7f9686093259c7f844f79e8d2146
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-12 12:40:16 +0100

    ew

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -26,21 +25,6 @@ function App() {
     return (
         <div className="App">
             <Header user={user} />
-            {!user ? (
-                <a
-                    href={`https://testing-mauve-five.vercel.app/auth/google`}
-                    className="waves-effect waves-light btn"
-                >
-                    login
-                </a>
-            ) : (
-                <a
-                    href={`https://testing-mauve-five.vercel.app/api/logout`}
-                    className="waves-effect waves-light btn"
-                >
-                    logout
-                </a>
-            )}
         </div>
     );
 }
