ef58eb43c990e9d6cbd1258f8867c571b31005af
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-18 15:56:11 +0100

    back to routes

--- src/reducers/getUser.js
+++ src/reducers/getUser.js
@@ -7,7 +7,6 @@ const getUser = (state = null, action) => {
     switch (action.type) {
         case GET_USER:
             return action.payload || false;
-
         default:
             return state;
     }
