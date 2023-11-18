8f8365b7f0cd1fa3287b7cb32a34bc8528ca2ac3
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-18 19:10:25 +0100

    add redux browser tool

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware, compose } from "redux";
+import {
+    legacy_createStore as createStore,
+    applyMiddleware,
+    compose,
+} from "redux";
 
 import reducers from "./reducers";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
