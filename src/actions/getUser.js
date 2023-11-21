fcfe3183d98fed74c39a6b51f4f0bf743e5ec813
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-21 12:02:34 +0100

    delete old link

--- src/actions/getUser.js
+++ src/actions/getUser.js
@@ -3,16 +3,16 @@ import { GET_USER } from "../constants/types";
 const getUser = () => async (dispatch) => {
     let response;
 
-    if (process.env.NODE_ENV === "development") {
-        response = await axios.get(`/api/current_user`);
-    } else {
-        response = await axios.get(
-            `https://testing-mauve-five.vercel.app/api/current_user`,
-            {
-                withCredentials: true,
-            }
-        );
-    }
+    // if (process.env.NODE_ENV === "development") {
+    //     response = await axios.get(`/api/current_user`);
+    // } else {
+    //     response = await axios.get(
+    //         `https://testing-mauve-five.vercel.app/api/current_user`,
+    //         {
+    //             withCredentials: true,
+    //         }
+    //     );
+    // }
 
     console.log("--------------------------response");
     console.log("--------------------------response");
